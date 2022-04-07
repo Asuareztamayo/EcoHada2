6902fd3dbbb8703253e96d4ab09242f1edbf6a5a
alvaro <bernabeyalvaro@gmail.com>
2022-04-07 16:26:31 +0200

    v 1.0.0 funciona el login

---
+++ index.js
@@ -0,0 +1,41 @@
+let email;
+let entrar2;
+let id = 1;
+
+async function acceder (){
+    email = document.getElementById("email").value;
+    entrar2 = document.getElementById("entrar2").value;
+    let a = email.substring(0,6);
+    let r;
+    if("alumno" == a){
+        await fetch(`http://localhost:8080/alumno/iniciarSesion?email=${email}&&password=${entrar2}`, {
+            method: 'POST',
+            headers:{
+                'Content-Type': 'application/json'
+            }
+            }).then(res => res.json())
+            .catch(error => console.error('Error:', error))
+            .then( response => { 
+                r = response;});
+            if(r > 0){
+            sessionStorage.setItem("id", r);
+            window.location.assign("vistasAlumno/alumno.html");
+            }
+            
+
+        }else{
+            await fetch(`http://localhost:8080/profesor/iniciarSesion?email=${email}&&password=${entrar2}`, {
+                method: 'POST',
+                headers:{
+                    'Content-Type': 'application/json'
+                }
+                }).then(res => res.json())
+                .catch(error => console.error('Error:', error))
+                .then( response => { 
+                    r = response;});
+                if(r > 0){
+                sessionStorage.setItem("id", r);
+                window.location.assign("vistasProfe/principalProfe.html");
+                }
+        }
+}
