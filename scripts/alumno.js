7758c31d6da160517c90cfb9ae7044fd20e03a55
Andrea Suarez <asuareztamayo@hotmail.com>
2022-04-08 16:31:27 +0200

    traer retos bd

--- scripts/alumno.js
+++ scripts/alumno.js
@@ -1,55 +1,6 @@
-//funcion inicial para cargar datos
 let alumno;
-document.addEventListener("DOMContentLoaded", async function () {
-let id = sessionStorage.getItem("id");
-await fetch(`http://localhost:8080/alumno/find${id}`)
-.then(res => res.json())
-.catch(error => console.error('Error:', error))
-.then(response => {
-alumno = response});
-console.log(alumno);
-document.getElementById('nAlumno').innerHTML += " " + alumno.name;
-document.getElementById('xp').innerHTML = alumno.xp;
-sessionStorage.setItem("alumno", alumno);
-asignarlvl();
-});
-//funcion inicial para cargar datos
-function asignarlvl(){
-    document.getElementById('categoria');
-    let lvls = ["https://i.ibb.co/Pw0gMvC/penguin-42936-640.png","https://i.ibb.co/yNqzZ38/pug-4862083-640.png",
-    "https://i.ibb.co/NyFkY4t/parrot-1417286-640.png","https://i.ibb.co/55VfBBd/shark-1454245-640.png",
-    "https://i.ibb.co/jbKxynY/sea-turtle-3199593-640.png"];
-    let subt = ["Pinguino limpia playas","Perro come restos","Pajaro Planta Arboles","Tiburon debora basura","ECO TORTUGA"];
-
-            for (let index = 0; index < lvls.length; index++) {
-                if(alumno.xp >= 0 && alumno.xp <50){
-                    document.getElementById('categoria').innerHTML += 
-                    `<img style="width: 250px; height:300px;" src="https://i.ibb.co/Pw0gMvC/penguin-42936-640.png"" alt="Lvl" border="0">
-                    <div class="d-flex flex-column">
-                        <h3>Pinguino limpia playas</h3>
-                        <h5>Te faltan ${50- alumno.xp} xp para el siguiente nivel.</h5>
-                    </div>`;
-                    break;
-                }else if(alumno.xp >= (index * 100) - 50){
-                    document.getElementById('categoria').innerHTML = "";
-                    document.getElementById('categoria').innerHTML += 
-                    `<img style="width: 250px; height:300px;" src="${lvls[index]}" alt="Lvl" border="0">
-                    <div class="d-flex flex-column">
-                        <h3>${subt[index]}</h3>
-                        <h5>Te faltan ${(((index+1) * 100) - 50)- alumno.xp} xp para el siguiente nivel.</h5>
-                    </div>`;
-                }else{
-                    break;
-                }
-                
-            
-            }
-        
-    
-}
-
-
-let principal = document.getElementById("home");
+let retosbd;
+let home = document.getElementById("home");
 let mainPrincipal = document.getElementById("principalAlumno");
 let mainRetos = document.getElementById("mainRetosActuales");
 let mainIndividual = document.getElementById("mainRetoIndividual");
@@ -57,7 +8,6 @@ let mainHecho = document.getElementById("mainRetosHechos");
 let irRetos = document.getElementById("retosActuales");
 // let irVoluntarios = document.getElementById("retosVoluntarios");
 let irTerminados = document.getElementById("retosCompletos");
-let retoIndividual = document.getElementsByClassName("btnReto");
 let consejos = [
     "Separa les escombraries.",
     "Apaga els llums.",
@@ -89,6 +39,88 @@ irTerminados.addEventListener("click", () => {
 // retoIndividual.addEventListener("click", () => {
 // //  verRetoIndividual();
 // });
+
+//funcion inicial para cargar datos
+document.addEventListener("DOMContentLoaded", async function () {
+    let id = sessionStorage.getItem("id");
+    await fetch(`http://localhost:8080/alumno/find${id}`)
+        .then((res) => res.json())
+        .catch((error) => console.error("Error:", error))
+        .then((response) => {
+            alumno = response;
+        });
+    console.log(alumno);
+    document.getElementById("nAlumno").innerHTML += " " + alumno.name;
+    document.getElementById("xp").innerHTML = alumno.xp;
+    sessionStorage.setItem("alumno", alumno);
+    asignarlvl();
+    verRetoAlumno();
+});
+//funcion inicial para cargar datos
+
+function asignarlvl() {
+    document.getElementById("categoria");
+    let lvls = [
+        "https://i.ibb.co/Pw0gMvC/penguin-42936-640.png",
+        "https://i.ibb.co/yNqzZ38/pug-4862083-640.png",
+        "https://i.ibb.co/NyFkY4t/parrot-1417286-640.png",
+        "https://i.ibb.co/55VfBBd/shark-1454245-640.png",
+        "https://i.ibb.co/jbKxynY/sea-turtle-3199593-640.png",
+    ];
+    let subt = [
+        "Pinguino limpia playas",
+        "Perro come restos",
+        "Pajaro Planta Arboles",
+        "Tiburon debora basura",
+        "ECO TORTUGA",
+    ];
+
+    for (let index = 0; index < lvls.length; index++) {
+        if (alumno.xp >= 0 && alumno.xp < 50) {
+            document.getElementById(
+                "categoria"
+            ).innerHTML += `<img style="width: 250px; height:300px;" src="https://i.ibb.co/Pw0gMvC/penguin-42936-640.png"" alt="Lvl" border="0">
+                    <div class="d-flex flex-column">
+                        <h3>Pinguino limpia playas</h3>
+                        <h5>Te faltan ${50 - alumno.xp
+                } xp para el siguiente nivel.</h5>
+                    </div>`;
+            break;
+        } else if (alumno.xp >= index * 100 - 50) {
+            document.getElementById("categoria").innerHTML = "";
+            document.getElementById(
+                "categoria"
+            ).innerHTML += `<img style="width: 250px; height:300px;" src="${lvls[index]
+                }" alt="Lvl" border="0">
+                    <div class="d-flex flex-column">
+                        <h3>${subt[index]}</h3>
+                        <h5>Te faltan ${(index + 1) * 100 - 50 - alumno.xp
+                } xp para el siguiente nivel.</h5>
+                    </div>`;
+        } else {
+            break;
+        }
+    }
+}
+
+function verRetoAlumno() {
+    let retos = document.getElementById("retos");
+    retosbd = alumno.tareas;
+    console.log(retosbd);
+    for (let i = 0; i < retosbd.lenght; i++) {
+        retos += `<div class="col-6">
+                    <div class="d-flex flex-row m-4 retoHecho">
+                        <h3 class="col-8">${retosbd[i].text}</h3>
+                    <div class="col-4 d-flex justify-content-end imgreto">
+                    <img src="${retosbd[i].insignea.img}" alt="Medalla"
+                    border="0">
+                    </div>
+                    <div class="d-flex justify-content-end m-2">
+                        <button onclick=verRetoIndividual(${retosbd[i].id})" class="btnReto"> + Info</button>
+                    </div>
+                </div>`;
+    }
+}
 
 function irPrincipal() {
     if (mainPrincipal.style.display == "none") {
@@ -143,7 +175,6 @@ function verRetoIndividual() {
 
 function footer() {
     let verConsejo = consejos[Math.floor(Math.random() * consejos.length)];
-    console.log("El consejo es " + verConsejo);
     document.getElementById("footer").innerHTML = verConsejo;
 }
 
