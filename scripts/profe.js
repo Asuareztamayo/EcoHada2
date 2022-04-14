a2d1540f1828b7e48449954c0988103e575c029a
alvaro <bernabeyalvaro@gmail.com>
2022-04-14 17:12:04 +0200

    funcionando alumnos profesores

--- scripts/profe.js
+++ scripts/profe.js
@@ -1,63 +1,94 @@
 let consejos = [
-    "Separa les escombraries.",
-    "Apaga els llums.",
-    "Desendolla el que no s'estigui fent servir.",
-    "Tanca correctament les aixetes.",
-    "Utilitza bosses reutilitzables.",
-    "Utilitza bombetes LED.",
-    "Reutilitza.",
-    "Evita utilitzar piles.",
-    "Evita els aerosols.",
-    "Consumeix productes de temporada.",
-    "Utilitza el transport públic.",
-    "Porta els medicaments caducats a la farmàcia.",
-    "Aneu caminant sempre que pugueu.",
-    "Tanca la aixeta quan et rentis els dents.",
-    "Estableix un temps de dutxa.",
-    "Surt mes a donar una volta, utilitza menys la tecología.",
-    "Utilitza recipents de vidre, en comptes dels de plàstic.",
-    "No llencis qualsevol líquid pel desguàs, l'aigua es contamina molt fàcilment.",
-    "Si vas de passeig, recorda't d'emportar-te les restes.",
-    "Sabies que l`aigua de bullir es pot utilitzar per regar plantes? fins i tot és millor."
+  "Separa les escombraries.",
+  "Apaga els llums.",
+  "Desendolla el que no s'estigui fent servir.",
+  "Tanca correctament les aixetes.",
+  "Utilitza bosses reutilitzables.",
+  "Utilitza bombetes LED.",
+  "Reutilitza.",
+  "Evita utilitzar piles.",
+  "Evita els aerosols.",
+  "Consumeix productes de temporada.",
+  "Utilitza el transport públic.",
+  "Porta els medicaments caducats a la farmàcia.",
+  "Aneu caminant sempre que pugueu.",
+  "Tanca la aixeta quan et rentis els dents.",
+  "Estableix un temps de dutxa.",
+  "Surt mes a donar una volta, utilitza menys la tecología.",
+  "Utilitza recipents de vidre, en comptes dels de plàstic.",
+  "No llencis qualsevol líquid pel desguàs, l'aigua es contamina molt fàcilment.",
+  "Si vas de passeig, recorda't d'emportar-te les restes.",
+  "Sabies que l`aigua de bullir es pot utilitzar per regar plantes? fins i tot és millor.",
 ];
 //funcion para reconocer al profesor
 let profe;
 let cursos = [];
+let alumnos = [];
 document.addEventListener("DOMContentLoaded", async function () {
-    let id = sessionStorage.getItem("id");
-    await fetch(`http://localhost:8080/profesor/find${id}`)
-        .then(res => res.json())
-        .catch(error => console.error('Error:', error))
-        .then(response => {
-            profe = response
-        });
-    console.log(profe);
-    document.getElementById('nProfe').innerHTML += " " + profe.name;
-    document.getElementById("nomProfe").innerHTML += profe.name;
-    document.getElementById("apProfe").innerHTML += profe.lastName;
-    document.getElementById("emailProfe").innerHTML += profe.email;
-    document.getElementById("contraProfe").innerHTML += profe.password;
-    sessionStorage.setItem("profe", profe);
-    await fetch(`http://localhost:8080/curso/profesor${id}`)
-        .then(res => res.json())
-        .catch(error => console.error('Error:', error))
-        .then(response => {
-            cursos = response
-        });
-    console.log(cursos);
-    for (let i = 0; i < cursos.length; i++){
-        document.getElementById("cursos").innerHTML += `<button class="grups" onclick="abrircurso(${i})" style="width:200px;"><b>${cursos[i].name}</b></button>`
-       }
+  let id = sessionStorage.getItem("id");
+  await fetch(`http://localhost:8080/profesor/find${id}`)
+    .then((res) => res.json())
+    .catch((error) => console.error("Error:", error))
+    .then((response) => {
+      profe = response;
+    });
+  console.log(profe);
+  document.getElementById("nProfe").innerHTML += " " + profe.name;
+  document.getElementById("nomProfe").innerHTML += profe.name;
+  document.getElementById("apProfe").innerHTML += profe.lastName;
+  document.getElementById("emailProfe").innerHTML += profe.email;
+  document.getElementById("contraProfe").innerHTML += profe.password;
+  sessionStorage.setItem("profe", profe);
+  await fetch(`http://localhost:8080/curso/profesor${id}`)
+    .then((res) => res.json())
+    .catch((error) => console.error("Error:", error))
+    .then((response) => {
+      cursos = response;
+    });
+  console.log(cursos);
+  for (let i = 0; i < cursos.length; i++) {
+    document.getElementById(
+      "cursos"
+    ).innerHTML += `<button class="grups" onclick="abrircurso(${i})" style="width:200px;"><b>${cursos[i].name}</b></button>`;
+  }
 });
 // fin funcion para reconocer al profesor
 
 function footer() {
-    let verConsejo = consejos[Math.floor(Math.random() * consejos.length)];
-    document.getElementById("footer").innerHTML = verConsejo;
+  let verConsejo = consejos[Math.floor(Math.random() * consejos.length)];
+  document.getElementById("footer").innerHTML = verConsejo;
 }
 
 let cambioConsejo = setInterval(footer, 5000);
 
-function abrircurso(index) {
-    document.getElementById("holas").innerHTML = cursos[index].name;
+async function abrircurso(index) {
+  document.getElementById("holas").innerHTML = cursos[index].name;
+  await fetch(`http://localhost:8080/curso/alumno${cursos[index].id}`)
+    .then((res) => res.json())
+    .catch((error) => console.error("Error:", error))
+    .then((response) => {
+      alumnos = response;
+    });
+  console.log(alumnos);
+  document.getElementById("listAlumnos").innerHTML = "";
+  document.getElementById("listAlumnos2").innerHTML = "";
+  for (let i = 1; i < alumnos.length + 1; i++) {
+    if (i % 2 == 0) {
+      document.getElementById("listAlumnos").innerHTML += `<div class="d-flex f-row">
+            <h5 class="listAl2">${
+              alumnos[i - 1].lastName + " " + alumnos[i - 1].name
+            }</h5>
+            <span class="iconify" data-icon="bx:pencil" data-width="25"></span>
+            <span class="iconify" data-icon="carbon:user-avatar-filled-alt" data-width="25"></span>
+            </div>`;
+    } else {
+      document.getElementById("listAlumnos2").innerHTML += `<div class="d-flex f-row">
+            <h5 class="listAl2">${
+              alumnos[i - 1].lastName + " " + alumnos[i - 1].name
+            }</h5>
+            <span class="iconify" data-icon="bx:pencil" data-width="25"></span>
+            <span class="iconify" data-icon="carbon:user-avatar-filled-alt" data-width="25"></span>
+            </div>`;
+    }
+  }
 }
